27ac14cb08215ebaa1320bddfe90fd691c81a77a
Rezwana91 <55046929+Rezwana91@users.noreply.github.com>
2019-10-06 10:57:56 +0600

    Update customersUi.cs

--- customersUi.cs
+++ customersUi.cs
@@ -149,6 +149,24 @@ namespace customer
 
             }
         }
+        private void gridStateZone_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            LoadSerial(gridStateZone);
+        }
+
+        private void LoadSerial(object gridStateZone)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSerial(DataGridView grid)
+        {
+            foreach (DataGridViewRow row in grid.Rows)
+            {
+                grid.Rows[row.Index].HeaderCell.Value = string.Format("{0}  ", row.Index + 1).ToString();
+                row.Height = 25;
+            }
+        }
 
        
     }
